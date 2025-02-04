54589ad40ed097c0badcba347af9f4c278cb7be5
Anton Obukhov <anton.obukhov@gmail.com>
2025-02-04 02:02:28 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(
     description="A Python package for dual-vision processing in Gradio.",
     url="https://github.com/toshas/gradio-dualvision",
     license="Apache-2.0",
-    packages=find_packages(),
+    packages=find_packages(include=["gradio_dualvision", "gradio_dualvision.*"]),
     install_requires=requirements,
     python_requires=">=3.8",
+    include_package_data=True,
 )
