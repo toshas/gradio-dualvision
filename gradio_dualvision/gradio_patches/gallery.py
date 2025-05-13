9342eb91a738b64624aee0a54a4e94a16261e44c
Anton Obukhov <anton.obukhov@gmail.com>
2025-05-13 14:19:03 +0200

    wip gradio 5

--- gradio_dualvision/gradio_patches/gallery.py
+++ gradio_dualvision/gradio_patches/gallery.py
@@ -1,41 +1,48 @@
 from __future__ import annotations
 
 from concurrent.futures import ThreadPoolExecutor
-from gradio.components.gallery import (
-    GalleryImageType,
-    CaptionedGalleryImageType,
-    GalleryImage,
-    GalleryData,
-)
+
+from gradio_client import utils as client_utils
+from gradio.components.gallery import GalleryImage, GalleryData, GalleryMediaType, CaptionedGalleryMediaType, GalleryVideo
 from pathlib import Path
-from urllib.parse import urlparse
+from urllib.parse import quote, urlparse
 
 import gradio
 import numpy as np
 import PIL.Image
 from gradio_client.utils import is_http_url_like
 
-from gradio import processing_utils, utils, wasm_utils
-from gradio.data_classes import FileData
+from gradio import processing_utils, utils, wasm_utils, image_utils
+from gradio.data_classes import FileData, ImageData
 
 
 class Gallery(gradio.Gallery):
     def postprocess(
-        self,
-        value: list[GalleryImageType | CaptionedGalleryImageType] | None,
+            self,
+            value: list[GalleryMediaType | CaptionedGalleryMediaType] | None,
     ) -> GalleryData:
         """
-        This is a patched version of the original function, wherein the format for PIL is computed based on the data type:
-        format = "png" if img.mode == "I;16" else "webp"
-        """
+            This is a patched version of the original function, wherein the format for PIL is computed based on the data type:
+            format = "png" if img.mode == "I;16" else "webp"
+
+            Parameters:
+                value: Expects the function to return a `list` of images or videos, or `list` of (media, `str` caption) tuples. Each image can be a `str` file path, a `numpy` array, or a `PIL.Image` object. Each video can be a `str` file path.
+            Returns:
+                a list of images or videos, or list of (media, caption) tuples
+            """
         if value is None:
             return GalleryData(root=[])
+        if isinstance(value, str):
+            raise ValueError(
+                "The `value` passed into `gr.Gallery` must be a list of images or videos, or list of (media, caption) tuples."
+            )
         output = []
 
         def _save(img):
             url = None
             caption = None
             orig_name = None
+            mime_type = None
             if isinstance(img, (tuple, list)):
                 img, caption = img
             if isinstance(img, np.ndarray):
@@ -44,28 +51,52 @@ class Gallery(gradio.Gallery):
                 )
                 file_path = str(utils.abspath(file))
             elif isinstance(img, PIL.Image.Image):
-                format = "png" if img.mode == "I;16" else "webp"
+                format = "png" if img.mode == "I;16" else "webp"  # Patch 1: change format based on the inbound dtype
                 file = processing_utils.save_pil_to_cache(
                     img, cache_dir=self.GRADIO_CACHE, format=format
                 )
                 file_path = str(utils.abspath(file))
             elif isinstance(img, str):
-                file_path = img
-                if is_http_url_like(img):
+                mime_type = client_utils.get_mimetype(img)
+                if img.lower().endswith(".svg"):
+                    svg_content = image_utils.extract_svg_content(img)
+                    orig_name = Path(img).name
+                    url = f"data:image/svg+xml,{quote(svg_content)}"
+                    file_path = None
+                elif is_http_url_like(img):
                     url = img
                     orig_name = Path(urlparse(img).path).name
+                    file_path = img
                 else:
                     url = None
                     orig_name = Path(img).name
+                    file_path = img
             elif isinstance(img, Path):
                 file_path = str(img)
                 orig_name = img.name
+                mime_type = client_utils.get_mimetype(file_path)
             else:
                 raise ValueError(f"Cannot process type as image: {type(img)}")
-            return GalleryImage(
-                image=FileData(path=file_path, url=url, orig_name=orig_name),
-                caption=caption,
-            )
+            if mime_type is not None and "video" in mime_type:
+                return GalleryVideo(
+                    video=FileData(
+                        path=file_path,  # type: ignore
+                        url=url,
+                        orig_name=orig_name,
+                        mime_type=mime_type,
+                    ),
+                    caption=caption,
+                )
+            else:
+                return GalleryImage(
+                    image=ImageData(
+                        path=file_path,
+                        url=url,
+                        orig_name=orig_name,
+                        mime_type=mime_type,
+                    ),
+                    caption=caption,
+                )
 
         if wasm_utils.IS_WASM:
             for img in value:
