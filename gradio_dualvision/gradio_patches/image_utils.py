fix rotation due to invalid exif check order

gradio_dualvision/gradio_patches/image_utils.py
@@ -29,6 +29,7 @@ def patched_preprocess_image(
 ) -> np.ndarray | PIL.Image.Image | str | None:
     if payload is None:
         return payload
+
     if payload.url and payload.url.startswith("data:"):
         if type == "pil":
             print("Preprocessing payload as PIL image")
@@ -39,9 +40,12 @@ def patched_preprocess_image(
         elif type == "filepath":
             print("Preprocessing payload as file path")
             return decode_base64_to_file(payload.url, cache_dir, format)
+
     if payload.path is None:
         raise ValueError("Image path is None.")
+
     file_path = Path(payload.path)
+
     if payload.orig_name:
         p = Path(payload.orig_name)
         name = p.stem
@@ -57,29 +61,35 @@ def patched_preprocess_image(
             return str(file_path)
         raise Error("SVG files are not supported as input images for this app.")
 
-    # Check for heif or heic suffix, treat it as a special case and drop resolution immediately
-    if suffix.lower() in ["heif", "heic"] and type == "filepath":
-        im = PIL.Image.open(file_path).convert("RGB")
-        scale = min(1.0, 1024 / max(im.width, im.height))
-        im = im.resize((round(im.width * scale), round(im.height * scale)), PIL.Image.BILINEAR)
-        file_path = processing_utils.save_pil_to_cache(im, cache_dir=cache_dir)
-
     im = PIL.Image.open(file_path)
+    exif = im.getexif()
+
+    if suffix.lower() in ["heif", "heic"] and type == "filepath":
+        im = im.convert("RGB")
+
+    if exif.get(274, 1) != 1 and hasattr(ImageOps, "exif_transpose"):
+        # 274 is the code for image rotation and 1 means "correct orientation"
+        try:
+            im = ImageOps.exif_transpose(im)
+            Path(file_path).resolve().write_bytes(processing_utils.encode_pil_to_bytes(im, format="webp"))
+        except Exception:
+            warnings.warn(f"Failed to transpose image {file_path} based on EXIF data.")
+
+    max_dim = max(im.width, im.height)
+    if max_dim > 2048:
+        scale = min(1.0, 2048 / max_dim)
+        im = im.resize((round(im.width * scale), round(im.height * scale)), PIL.Image.BILINEAR)
+        Path(file_path).resolve().write_bytes(processing_utils.encode_pil_to_bytes(im, format="webp"))
+
     if type == "filepath" and (image_mode in [None, im.mode]):
         return str(file_path)
 
-    exif = im.getexif()
-    # 274 is the code for image rotation and 1 means "correct orientation"
-    if exif.get(274, 1) != 1 and hasattr(ImageOps, "exif_transpose"):
-        try:
-            im = ImageOps.exif_transpose(im)
-        except Exception:
-            warnings.warn(f"Failed to transpose image {file_path} based on EXIF data.")
     if suffix.lower() != "gif" and im is not None:
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             if image_mode is not None:
                 im = im.convert(image_mode)
+
     return format_image(
         im,
         type=cast(Literal["numpy", "pil", "filepath"], type),